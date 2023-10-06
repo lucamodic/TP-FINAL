2204834346075a564ae2dd878ac8927a485a6982
Usuario <pablot.pavan@gmail.com>
2023-10-06 13:16:15 -0300

    agrego sql

---
+++ bdd.sql
@@ -0,0 +1,11 @@
+CREATE SCHEMA IF NOT EXISTS respuestados;
+USE respuestados;
+CREATE TABLE user(
+    name VARCHAR(255),
+    spawn VARCHAR(255),
+    sex VARCHAR(255),
+    mail VARCHAR(255),
+    password VARCHAR(255),
+    username VARCHAR(255),
+    image VARCHAR(255)
+);
